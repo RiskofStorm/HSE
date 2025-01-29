ae10d2e0d76cb53f374d909ef3ba484a35d30737
riskofstorm <riskofstorm@gmail.com>
2025-01-30 00:45:15 +0300

    done hw_3 >_<

--- etl/hw_2_nifi/docker_run.ps1
+++ etl/hw_2_nifi/docker_run.ps1
@@ -1,10 +1,6 @@
-docker run --name nifi \
-  -p 8443:8443 \
-   -v S:\\repo_volumes\\hse\\etl\\nifi-files \
-  -d \
-  -e SINGLE_USER_CREDENTIALS_USERNAME=admin \
-  -e SINGLE_USER_CREDENTIALS_PASSWORD=ctsBtRBKHRAx69EqUghvvgEvjnaLjFEB \
-  apache/nifi:latest
+docker run --name nifi -p 8443:8443 -v /s/repo_volumes/hse/etl/nifi-files:/tmp/nifi-files -d -e SINGLE_USER_CREDENTIALS_USERNAME=admin -e SINGLE_USER_CREDENTIALS_PASSWORD=ctsBtRBKHRAx69EqUghvvgEvjnaLjFEB apache/nifi:latest
 
 
-  # https://localhost:8443
+# https://localhost:8443
+
+# pets-data.json
