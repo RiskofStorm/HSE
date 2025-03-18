34da07738b246af521b90a039f3053e7ed84a8c2
riskofstorm <riskofstorm@gmail.com>
2025-03-18 22:25:42 +0300

    fixed typo

--- etl/hw_5_final/dags/datamart.py
+++ etl/hw_5_final/dags/datamart.py
@@ -38,38 +38,38 @@ def datamart_update(logger) -> None:
 
     logger.info('STARTED LOADING DATA FROM MONGO TO PG')
     with pg_conn.cursor() as cursor:
-
-            cursor.execute(
-                query="""
-                    CREATE OR REPLACE VIEW cdm_mdb.products_history AS
-                    SELECT product_id,
-                           date::TIMESTAMP AS update_dttm,
-                          current_price,
-                           price AS previous_price,
-                           current_price - price::bigint AS diff_price
-                    FROM (
-                    select t.product_id, 
-                           j.value::JSON ->> 'date' AS date,
-                           j.value::JSON ->> 'price' AS price,
-                           t.current_price 
-                    from stg_mdb.product_price_history t
-                    cross join lateral jsonb_array_elements_text(
-                        price_changes) j
-                    ) t
-                    ORDER BY product_id, update_dttm DESC;
+        cursor.execute(
+            query="""
+                CREATE OR REPLACE VIEW cdm_mdb.products_history AS
+                SELECT product_id,
+                       date::TIMESTAMP AS update_dttm,
+                       current_price,
+                       price AS previous_price,
+                       current_price - price::bigint AS diff_price
+                FROM (
+                        SELECT t.product_id, 
+                               j.value::JSON ->> 'date' AS date,
+                               j.value::JSON ->> 'price' AS price,
+                               t.current_price 
+                        FROM stg_mdb.product_price_history t
+                        CROSS JOIN LATERAL JSONB_ARRAY_ELEMENTS_TEXT(
+                            price_changes
+                    ) j
+                ) t
+                ORDER BY product_id, update_dttm DESC;
+                
+                
+                
+                CREATE OR REPLACE VIEW cdm_mdb.products_avg_changes
+                SELECT product_id,
+                       current_price,
+                       avg(diff_price) AS avg_diff_price
                     
-                    
-                    
-                    CREATE OR REPLACE VIEW cdm_mdb.products_avg_changes
-                    SELECT product_id,
-                           current_price,
-                           avg(diff_price) AS avg_diff_price
-                        
-                    FROM cdm_mdb.products_history
-                    GROUP BY product_id, current_price;
-                    
-                """
-            )
+                FROM cdm_mdb.products_history
+                GROUP BY product_id, current_price;
+                
+            """
+        )
 
 
         pg_conn.commit()
